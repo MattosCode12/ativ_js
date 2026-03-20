066b5321e994d3e2a37a7e72ff9cf4491147171e
MattosCode12 <enzo_mattos@estudante.sesisenai.org.br>
2026-03-20 08:47:10 -0300

    ADD - Exercicio 3 finalizado.

---
+++ exercicio_03.js
@@ -0,0 +1,15 @@
+function exercicio_03(){
+
+let gasto = 0;
+let ganho = 0;
+let saldo = 0;
+
+for(let i = 1; i<= 12; i++){
+    gasto[i] += parseFloat(prompt("Informe o gasto bruto do mes: "+ i))
+    ganho[i] += parseFloat(prompt("Informe o ganho bruto do mes: "+ i))
+}
+saldo+bruto-gasto
+alert("O saldo financeiro anual é de: R%"+ saldo);
+alert("O ganho bruto anual é de: R%"+ bruto);
+alert("O gasto anual é de: R%"+ gasto);
+}
