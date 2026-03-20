96c2b6a36b49fbf92e8d0190eae88d2366c14b8d
MattosCode12 <enzo_mattos@estudante.sesisenai.org.br>
2026-03-20 09:15:05 -0300

    ADD - Exercicio 6 finalizado.

---
+++ exercicio_06.js
@@ -0,0 +1,12 @@
+function exercicio_06(){
+let letra;
+
+letra=prompt("Informe uma letra");
+
+if(letra === 'a' || letra === 'e' || letra === 'i' || letra === 'o' || letra === 'u'){
+    alert(" A letra é uma vogal.");
+} else {
+    alert("A letra é uma consoante.")
+}
+}
+
