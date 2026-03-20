b84a4908f2d5b4b6754ea75ab2e1fa915f5cd5fd
MattosCode12 <enzo_mattos@estudante.sesisenai.org.br>
2026-03-20 09:34:23 -0300

    ADD - Exercicio 7 finalizado.

---
+++ exercicio_07.js
@@ -0,0 +1,40 @@
+function exercicio_07(){
+let sabor = prompt("Escolha um sabor de picole: \nA-Chocolate \nB-Morango \nC-Creme \nD-Manga \nE-Melancia \nF-Vanilla ice \ nG-Céu Azul \nH-Brownie \nI-Hawaino")
+
+if(sabor === 'A'){
+    alert("Sabor Chocolate: R$1,50")
+    return;
+}
+if(sabor === 'B'){
+    alert("Sabor Morango: R$2,50")
+    return;
+}
+if(sabor === 'C'){
+    alert("Sabor Creme: R$2,50")
+    return;
+}
+if(sabor === 'D'){
+    alert("Sabor Manga: R$3,20")
+    return;
+}
+if(sabor === 'E'){
+    alert("Sabor Melancia: R$3,40")
+    return;
+}
+if(sabor === 'F'){
+    alert("Sabor Vanilla ice: R$3,00")
+    return;
+}
+if(sabor === 'G'){
+    alert("Sabor Céu Azul: R$3,60")
+    return;
+}
+if(sabor === 'H'){
+    alert("Sabor Brownie: R$4,00")
+    return;
+}
+if(sabor === 'I'){
+    alert("Sabor hawaino: R$5,00")
+    return;
+}
+}
